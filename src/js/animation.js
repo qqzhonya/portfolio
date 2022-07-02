b0c5ace183077899d035159747d402dcc1a51f29
qqzhonya <45445030+qqzhonya@users.noreply.github.com>
2022-07-02 22:43:47 +0300

    Small fixes for adaptive view. And make full animation playing.

--- src/js/animation.js
+++ src/js/animation.js
@@ -3,15 +3,6 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-// gsap.set(
-// 	'.editor',
-// 	{
-// 		perspective: 800,
-// 		transformStyle: 'preserve-3d',
-// 		autoAlpha: 1,
-// 		rotateX: 25
-// 	}
-// );
 
 gsap.set(
 	[
@@ -35,118 +26,129 @@ gsap.set(
 	}
 );
 
+gsap.set(
+	'.editor',
+	{
+		perspective: 800,
+		transformStyle: 'preserve-3d',
+		opacity: 0,
+		rotateX: 15
+	}
+);
+
 const tl = gsap.timeline({
 	defaults: {
 		duration: 1.5,
 	}
 });
 
-// tl.fromTo(
-// 	'.main-intro-elem', 
-// 	{opacity: 0, y: '25'}, 
-// 	{opacity: 1, y: '0', stagger: 1}
-// );
-// tl.to(
-// 	'.main-intro-elem', 
-// 	{duration: .5, opacity: '0', display: 'none'},
-// 	'+=2');
-// tl.fromTo(
-// 	'.editor',
-// 	{opacity: '0', scale: '1', rotateX: 25},
-// 	{opacity: '1', scale: '1', rotateX: 0},
-// 	'+=.5'
-// )
-// tl.fromTo(
-// 	'.editor-head',
-// 	{opacity: '0', y: -30, z: 80},
-// 	{opacity: '1', y: 0, z: 0},
-// 	'-=.5'
-// )
-// .fromTo(
-// 	'.editor-footer',
-// 	{opacity: '0', y: -50, z: 25},
-// 	{opacity: '1', y: 0, z: 0},
-// 	'<'
-// )
-// .fromTo(
-// 	'.editor-sidebar',
-// 	{opacity: '0', y: -30, z: 30},
-// 	{opacity: '1', y: 0, z: 0},
-// 	'-=.5'
-// )
-// .fromTo(
-// 	'.editor-sidebar-btn',
-// 	{opacity: '0', z: 10},
-// 	{duration: .5, opacity: '1', z: 0}
-// )
-// .fromTo(
-// 	'.editor-explorer',
-// 	{opacity: '0', scale: '1.05', z: 30},
-// 	{duration: .8, opacity: '1', scale: '1', z: 0},
-// 	'-=.4'
-// )
-// .fromTo(
-// 	'.editor-explorer-title',
-// 	{opacity: '0', scale: '1.03', z: 40},
-// 	{duration: .5, opacity: '1', scale: '1', z: 0},
-// )
-// .fromTo(
-// 	'.editor-explorer-elem',
-// 	{opacity: '0', z: 25},
-// 	{duration: .5, opacity: '1', z: 0, stagger: .3},
-// 	'-=.4'
-// )
-// .fromTo(
-// 	'.editor-terminal',
-// 	{opacity: '0', z: 25},
-// 	{opacity: '1', z: 0, stagger: .3},
-// 	'<'
-// )
-// .fromTo(
-// 	'.editor-tab-elem',
-// 	{opacity: '0', scale: '1.01', z: 25},
-// 	{opacity: '1', scale: '1', z: 0},
-// 	'<'
-// )
-// .fromTo(
-// 	'.editor-terminal-tab-btn',
-// 	{opacity: '0', z: 15},
-// 	{duration: .35, opacity: '1', z: 0, stagger: .2},
-// 	'<'
-// )
-// .fromTo(
-// 	'.editor-terminal-row',
-// 	{opacity: '0', z: 15},
-// 	{duration: .8, opacity: '1', z: 0, stagger: .2},
-// )
-// .fromTo(
-// 	'.editor-breadcrumbs',
-// 	{opacity: '0', z: 10},
-// 	{duration: .4, opacity: '1', z: 0},
-// 	'<'
-// )
-// .fromTo(
-// 	'.editor-breadcrumbs-elem',
-// 	{opacity: '0', scale: '1.02', z: 20},
-// 	{duration: .5, opacity: '1', z: 0, stagger: .3},
-// 	'-=.3'
-// )
-// .fromTo(
-// 	'.editor-field',
-// 	{opacity: '0', z: 20},
-// 	{opacity: '1', z: 0},
-// 	'-=.3'
-// )
-// .fromTo(
-// 	'.editor-field-row',
-// 	{opacity: '0', z: 20},
-// 	{duration: .5, opacity: '1', z: 0, stagger: .3}
-// )
-// .to(
-// 	'.editor',
-// 	{rotateX: 0},
-// 	'<'
-// );
+tl.fromTo(
+	'.main-intro-elem', 
+	{opacity: 0, y: '25'}, 
+	{opacity: 1, y: '0', stagger: 1}
+)
+.to(
+	'.main-intro-elem', 
+	{duration: .5, opacity: '0', display: 'none'},
+	'+=2')
+.to(
+	'.editor', 
+	{
+		opacity: 1
+	},
+	'+=.5'
+)
+.fromTo(
+	'.editor-head',
+	{opacity: '0', y: -30, z: 80},
+	{opacity: '1', y: 0, z: 0},
+	'+=.5'
+)
+.fromTo(
+	'.editor-footer',
+	{opacity: '0', y: -50, z: 25},
+	{opacity: '1', y: 0, z: 0},
+	'<'
+)
+.fromTo(
+	'.editor-sidebar',
+	{opacity: '0', y: -30, z: 30},
+	{opacity: '1', y: 0, z: 0},
+	'-=.5'
+)
+.fromTo(
+	'.editor-sidebar-btn',
+	{opacity: '0', z: 10},
+	{duration: .5, opacity: '1', z: 0}
+)
+.fromTo(
+	'.editor-explorer',
+	{opacity: '0', scale: '1.05', z: 30},
+	{duration: .8, opacity: '1', scale: '1', z: 0},
+	'-=.4'
+)
+.fromTo(
+	'.editor-explorer-title',
+	{opacity: '0', scale: '1.03', z: 40},
+	{duration: .5, opacity: '1', scale: '1', z: 0},
+)
+.fromTo(
+	'.editor-explorer-elem',
+	{opacity: '0', z: 25},
+	{duration: .5, opacity: '1', z: 0, stagger: .3},
+	'-=.4'
+)
+.fromTo(
+	'.editor-terminal',
+	{opacity: '0', z: 25},
+	{opacity: '1', z: 0, stagger: .3},
+	'<'
+)
+.fromTo(
+	'.editor-tab-elem',
+	{opacity: '0', scale: '1.01', z: 25},
+	{opacity: '1', scale: '1', z: 0},
+	'<'
+)
+.fromTo(
+	'.editor-terminal-tab-btn',
+	{opacity: '0', z: 15},
+	{duration: .35, opacity: '1', z: 0, stagger: .2},
+	'<'
+)
+.fromTo(
+	'.editor-terminal-row',
+	{opacity: '0', z: 15},
+	{duration: .8, opacity: '1', z: 0, stagger: .2},
+)
+.fromTo(
+	'.editor-breadcrumbs',
+	{opacity: '0', z: 10},
+	{duration: .4, opacity: '1', z: 0},
+	'<'
+)
+.fromTo(
+	'.editor-breadcrumbs-elem',
+	{opacity: '0', scale: '1.02', z: 20},
+	{duration: .5, opacity: '1', z: 0, stagger: .3},
+	'-=.3'
+)
+.fromTo(
+	'.editor-field',
+	{opacity: '0', z: 20},
+	{opacity: '1', z: 0},
+	'-=.3'
+)
+.fromTo(
+	'.editor-field-row',
+	{opacity: '0', z: 20},
+	{duration: .5, opacity: '1', z: 0, stagger: .3}
+)
+.to(
+	'.editor',
+	{rotateX: 0},
+	'<'
+);
 
 (function terminal() {
 	const terminalField = document.querySelector('.editor-terminal-field');
